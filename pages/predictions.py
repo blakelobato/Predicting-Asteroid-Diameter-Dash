853317b4e8cd26d1f37070c52f446f7164436196
blakelobato <blakelobato1@gmail.com>
2020-02-12 20:56:15 -0800

    works locally, trying to deply..

--- pages/predictions.py
+++ pages/predictions.py
@@ -6,10 +6,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import pandas as pd
 from joblib import load
-import shap
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#import matplotlib
+#matplotlib.use('Agg')
+#import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 import numpy
@@ -142,20 +141,20 @@ layout = html.Div(
 
 ####BRUNO NOTES#####
 
-def fig_to_uri(in_fig, close_all=True, **save_args):
-    """
-    Save a figure as a URI
-    :param in_fig:
-    :return:
-    """
-    out_img = BytesIO()
-    in_fig.savefig(out_img, format='png', **save_args)
-    if close_all:
-        in_fig.clf()
-        plt.close('all')
-    out_img.seek(0)  # rewind file
-    encoded = base64.b64encode(out_img.read()).decode("ascii").replace("\n", "")
-    return "data:image/png;base64,{}".format(encoded)
+# def fig_to_uri(in_fig, close_all=True, **save_args):
+#     """
+#     Save a figure as a URI
+#     :param in_fig:
+#     :return:
+#     """
+#     out_img = BytesIO()
+#     in_fig.savefig(out_img, format='png', **save_args)
+#     if close_all:
+#         in_fig.clf()
+#         plt.close('all')
+#     out_img.seek(0)  # rewind file
+#     encoded = base64.b64encode(out_img.read()).decode("ascii").replace("\n", "")
+#     return "data:image/png;base64,{}".format(encoded)
 
 @app.callback(
     Output('prediction-content', 'children'),
